fc63a2d5799b3db4b258fb2875b0f6bf798b97a8
Rifen <seth.a.gehring@gmail.com>
2020-11-18 11:43:32 -0700

    Edited pluginrc.zsh and env

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -2,3 +2,29 @@
 # Put here your functions, environment variables, aliases and whatever you need.
 
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+plugin_name='xxh-plugin-zsh-zinit'
+
+export ZSH="$CURR_DIR/.zinit"
+
+if [[ -v ZSH_THEME ]]; then
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo $plugin_name: Found ZSH_THEME=$ZSH_THEME
+  fi
+else
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo $plugin_name: Set default ZSH_THEME=agnoster
+  fi
+
+if [[ -v plugins ]]; then
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo $plugin_name: Found plugins=$plugins
+  fi
+else
+  if [[ $XXH_VERBOSE == '2' ]]; then
+    echo $plugin_name: Set default plugins=OMZ::plugins--git
+  fi
+  export plugins=(OMZ::plugins--git)
+fi
+
+source $CURR_DIR/.zinit/bin/zinit.zsh
+autoload -Uz _zinit && (( ${+_comps} )) && _comps[zinit]=_zinit && zinit self-update
